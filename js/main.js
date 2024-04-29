b3e796d42faec9dfdb0e6c66798d0dde37626e4a
RedStrikeRF <shubin152@gmail.com>
2024-04-29 12:57:30 +0500

    upd

--- js/main.js
+++ js/main.js
@@ -1,4 +1,33 @@
 document.addEventListener("DOMContentLoaded", function() {
+  const form = document.getElementById('myForm');
+  const loader = document.getElementById('loader');
+  const submitButton = document.getElementById('submitButton');
+  const answer = document.getElementById('answer');
+  const answer_result = document.querySelector('.answer_result');
+  const inputField = document.querySelector('#myFormInput');
+  const formAnwser = document.getElementById('form-url');
+
+  form.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    formAnwser.value = inputField.value; // Предотвращаем отправку формы по умолчанию
+    loader.style.display = 'block';
+    form.style.display = 'none'; // Показываем loader
+    submitButton.disabled = true; // Отключаем кнопку отправки, чтобы избежать повторных отправок
+    // answer_result.textContent = ...;Здесь должен быть ответ от вашей функции, оно переместит его в белое поле под формой
+    // Здесь вы можете выполнить ваш запрос куда-то
+    // Например, использовать fetch или отправку XMLHttpRequest
+    
+    // Пример с использованием setTimeout для имитации загрузки
+    setTimeout(function() {
+      // После завершения загрузки скрываем loader и включаем кнопку
+      loader.style.display = 'none';
+      submitButton.disabled = false;
+      answer.style.display = 'flex';
+      // Добавьте здесь обработку успешного ответа или ошибки
+    }, 3000); // Время имитации загрузки в миллисекундах
+  });
+
   const dropdownToggle = document.querySelector('.header-dropdown__toggle');
   const dropdownMenu = document.querySelector('.header-dropdown-menu');
   const dropDownText = document.querySelector('#dropdown-text');
